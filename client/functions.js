cea62b00708ed0f3c9acc5808ba56874eea91233
Janita Chalam <janitachalam@gmail.com>
2014-10-31 23:05:27 -0400

    updated manifest

--- client/functions.js
+++ client/functions.js
@@ -1,10 +1,12 @@
+var serverURL = "http://ader.klgilbert.com/";
+
+
 // Parse a URL. Based upon http://blog.stevenlevithan.com/archives/parseuri
 // parseUri 1.2.2, (c) Steven Levithan <stevenlevithan.com>, MIT License
 // Inputs: url: the URL you want to parse
 // Outputs: object containing all parts of |url| as attributes
+
 VERBOSE_DEBUG = true;
-
-
 parseUri = function(url) {
   var matches = /^(([^:]+(?::|$))(?:(?:\w+:)?\/\/)?(?:[^:@\/]*(?::[^:@\/]*)?@)?(([^:\/?#]*)(?::(\d*))?))((?:[^?#\/]*\/)*[^?#]*)(\?[^#]*)?(\#.*)?/.exec(url);
   // The key values are identical to the JS location object values for that key
@@ -15,6 +17,7 @@ parseUri = function(url) {
     uri[keys[i]] = matches[i] || "";
   return uri;
 };
+
 // Parses the search part of a URL into an key: value object.
 // e.g., ?hello=world&ext=adblock would become {hello:"world", ext:"adblock"}
 // Inputs: search: the search query of a URL. Must have &-separated values.
@@ -26,14 +29,66 @@ parseUri.parseSearch = function(search) {
   });
   return queryKeys;
 };
+
 // Strip third+ level domain names from the domain and return the result.
 // Inputs: domain: the domain that should be parsed
 //         keepDot: true if trailing dots should be preserved in the domain
 // Returns: the parsed domain
+
 parseUri.secondLevelDomainOnly = function(domain, keepDot) {
   var match = domain.match(/([^\.]+\.(?:co\.)?[^\.]+)\.?$/) || [domain, domain];
   return match[keepDot ? 0 : 1].toLowerCase();
 };
+
+function retrieveAds(adCount) {
+    //FIX THIS: THIS MUST ACCESS FILTERED AD OBJECT AND RETURN THE CORRECT NUMBER OF ADS
+    var ads = []; 
+    data = ["adCount": adCount];
+    $.getJSON("http://ader.klgilbert.com/", data, function(data) { 
+        //data will be an object of ad urls
+        $.each(data, function(index, value) {
+            //iterate through the data, append each ad url to the ads array
+            ads.push(value);         
+        });
+           
+    });
+    return ads;
+}
+
+function createAccount(username, password) {
+
+ data = ["username": username, "password": password];
+    
+ $.getJSON(serverURL, data, function(data) { /*data will be a web token which needs to get put in local storage. if returned error, return error*/  });
+  downloadAds();
+    
+}
+
+function loginAccount(username, password) {
+    
+    data = ["username": username, "password": password];
+    $.getJSON(serverURL, data, function(data) { /*data will be a web token which needs to get put in local storage. if returned error, return error*/  });
+                
+function saveAdPrefs(prefs) {
+ //save prefs in webtoken in local storage 
+    downloadAds();
+    filterAds();
+    
+}
+  
+function downloadAds() {
+ //download ads from database and put in local storage
+    
+}
+    
+function filterAds() {
+    
+    
+    
+}
+    
+    
+}
 
 
   
